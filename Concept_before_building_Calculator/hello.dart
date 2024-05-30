2db5533b66aef726e19acdf54b49f4720a27e415
Arjin Joshi <thearjinjoshi1@gmail.com>
2024-05-30 23:07:16 +0545

    [Basic concepts to build calculator]

---
+++ Concept_before_building_Calculator/hello.dart
@@ -0,0 +1,49 @@
+   // void main() {
+// print('hello');
+// }
+
+// integer and double concepts
+// void main() {
+//   int numOne = 20;
+//   int numTwo = 30;
+//   int sum = numOne + numTwo;
+//   int sub = numOne - numTwo;
+//   int mul = numOne * numTwo;
+//   double div = numOne / numTwo;
+//   print(numOne);
+//   print(numTwo);
+//   print(sum);
+//   print(sub);
+//   print(mul);
+//   print(div);
+//   print(div.ceil());
+// }
+
+// // string concepts
+// void main() {
+//   String a = 'Hello I am 20 yrs old till yesterday';
+//   print('$a');
+//   print(a.length); //shows total lenth spaces also count
+//   // consider it as a list 1st letter will be stored in index 0
+//   // index always starts from 0
+//   print(a[1]); //index 1 that means 2nd letter(e) will print
+
+//   // to show 1st 4 letter
+//   print(a.substring(0, 5));
+
+//   // to replace some letter with another eg here a is replaced by *
+//   print(a.replaceAll('e', '*'));
+// }
+
+// Let's learn about simple if and else concept
+
+void main() {
+  int numOne = 20;
+  const int numTwo = 30;
+  numOne = 40;
+  if (numOne > numTwo) {
+    print('yes');
+  } else {
+    print('no');
+  }
+}
